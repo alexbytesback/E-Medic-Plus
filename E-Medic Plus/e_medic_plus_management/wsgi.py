6a1f4b1d48b9243ad3c202c79511fef35c286596
Alex <92243577+alexbytesback@users.noreply.github.com>
2023-11-03 11:01:43 +0545

    Update wsgi.py

--- E-Medic Plus/e_medic_plus_management/wsgi.py
+++ E-Medic Plus/e_medic_plus_management/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_medic_plus_management.settings')
 
 application = get_wsgi_application()
+
+app = application
